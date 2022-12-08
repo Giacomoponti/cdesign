5d48d9580c19a2e930d988a5e02027649b0562df
Giacomoponti <giacomoponti00@gmail.com>
2022-12-08 16:37:26 +0100

    message

--- assignment/constprop.ml
+++ assignment/constprop.ml
@@ -129,7 +129,7 @@ module Fact =
       let empty_ref = ref UidM.empty in 
         let helper x y = (begin match x, y with 
                           |(SymConst.Const a, SymConst.Const b) -> if (a = b) then Some (SymConst.Const a) else Some (SymConst.NonConst)
-                          |(SymConst.Const _ , SymConst.NonConst) |(SymConst.NonConst, SymConst.Const _) |(SymConst.NonConst, SymConst.NonConst) -> Some SymConst.NonConst
+                          |(SymConst.Const _, SymConst.NonConst) |(SymConst.NonConst, SymConst.Const _) |(SymConst.NonConst, SymConst.NonConst) -> Some SymConst.NonConst
                           | _ -> Some SymConst.UndefConst
                           end) in 
         let u = (for i=0 to (List.length ds)-1 do  
@@ -159,7 +159,109 @@ let analyze (g:Cfg.t) : Graph.t =
 
 (* run constant propagation on a cfg given analysis results ----------------- *)
 (* HINT: your cp_block implementation will probably rely on several helper 
-   functions.                                                                 *)
+   functions. *)
+let cons_term uid t cb : (uid*Ll.terminator) = 
+let out_fact = cb uid in 
+  begin match t with 
+  | Ret (ty, Some (Id id)) -> begin match (try(UidM.find id out_fact) with Not_found -> SymConst.NonConst) with 
+                              |SymConst.Const c -> (uid,Ret(ty, Some (Const c))) 
+                              |_ -> (uid, t)
+                              end 
+  | Cbr (Id id, lbl_1, lbl_2) -> begin match (try(UidM.find id out_fact) with Not_found -> SymConst.NonConst)with 
+                                |SymConst.Const c ->(uid, Cbr(Const c, lbl_1, lbl_2))
+                                |_ -> (uid, t)
+                                end 
+  | _ -> (uid, t)           
+  end
+  
+let cons_ins uid t cb (id,insn) : (uid*Ll.insn) = 
+  let out_fact = cb id in 
+    let insn_ = 
+      (begin match insn with 
+      |Binop (bop, ty, op1, op2) ->  
+          begin match op1 with
+          |Id id1 -> begin match (try(UidM.find id1 out_fact) with Not_found -> SymConst.NonConst) with
+                      |SymConst.Const d -> begin match op2 with
+                                            |Id id2 -> begin match (try(UidM.find id2 out_fact) with Not_found -> SymConst.NonConst) with
+                                                      |SymConst.Const f -> Binop(bop, ty, Const d, Const f)
+                                                      |_ -> Binop(bop, ty, Const d, Id id2)
+                                                    end
+                                            |x -> Binop(bop, ty, Const d, x)
+                                            end
+                      |_ -> begin match op2 with 
+                            |Id id2 -> begin match (try(UidM.find id2 out_fact) with Not_found -> SymConst.NonConst) with
+                                        |SymConst.Const f -> Binop(bop, ty, Id id1, Const f)
+                                        |_ -> Binop(bop, ty, Id id1, Id id2)
+                                        end
+                            |x -> Binop(bop, ty, op1, x)
+                          end         
+                      end                  
+          |y -> begin match op2 with 
+                |Id id -> begin match (try(UidM.find id out_fact) with Not_found -> SymConst.NonConst) with
+                          |SymConst.Const f -> Binop(bop, ty, y, Const f)
+                          |_ -> Binop(bop, ty, y, Id id)
+                          end
+                |x -> Binop(bop, ty, y, x)
+                end
+          end 
+      |Store(ty, Id id, op2) -> 
+        begin match (try(UidM.find id out_fact) with Not_found ->SymConst.NonConst) with 
+          |SymConst.Const c -> Store(ty, Const c, op2)  
+          |_ -> insn
+        end
+      |Call(ty, op, tylist) -> Call(ty, op, 
+        List.map(fun (ty, op1) -> begin match op1 with
+                                  |Id c -> begin match (try(UidM.find c out_fact) with Not_found -> SymConst.NonConst) with
+                                      |SymConst.Const f -> (ty, Const f)
+                                      |_ -> (ty, op1)
+                                      end
+                                  | _ -> (ty,op1)
+                                  end) tylist) 
+      |Bitcast(ty1, Id id, ty2) -> 
+        begin match (try(UidM.find id out_fact) with Not_found -> SymConst.NonConst) with 
+          |SymConst.Const c -> Bitcast(ty1, Const c, ty2)  
+          |_ -> insn
+        end                            
+      |Icmp(cnd, ty, op1, op2) -> begin match op1 with
+                                  |Id id1 -> begin match (try(UidM.find id1 out_fact) with Not_found -> SymConst.NonConst ) with
+                                            |SymConst.Const d -> begin match op2 with
+                                                                  |Id id2 -> begin match (try(UidM.find id2 out_fact) with Not_found -> SymConst.NonConst) with
+                                                                            |SymConst.Const f -> Icmp(cnd, ty, Const d, Const f)
+                                                                            |_ -> Icmp(cnd, ty, Const d, Id id2)
+                                                                          end
+                                                                  |x -> Icmp(cnd, ty, Const d, x)
+                                                                
+                                                                end
+                                            |_ -> begin match op2 with 
+                                                    |Id id2 -> begin match (try(UidM.find id2 out_fact) with Not_found -> SymConst.NonConst)with
+                                                              |SymConst.Const f -> Icmp(cnd, ty, Id id1, Const f)
+                                                              |_ -> Icmp(cnd, ty, Id id1, Id id2)
+                                                            end
+                                                    |x -> Icmp(cnd, ty, op1, x)
+                                                    end         
+                                            end                  
+                                  |y -> begin match op2 with 
+                                        |Id id2 -> begin match (try(UidM.find id2 out_fact) with Not_found ->SymConst.NonConst) with
+                                                  |SymConst.Const f -> Icmp(cnd, ty, y, Const f)
+                                                  |_ -> Icmp(cnd, ty, y, Id id2)
+                                                  end
+                                        |x -> Icmp(cnd, ty, y, x)
+                                        end
+                                              
+                                  end
+      |_ -> insn                           
+      end) in (id, insn_)
+
+
+let cons_block (cb:uid -> Fact.t) (b:Ll.block) : Ll.block = 
+let ins_ls = b.insns in 
+  let (uid, t) = b.term in
+  let insns = List.map (cons_ins uid t cb) ins_ls in 
+    let term = cons_term uid t cb in 
+    {insns=insns; term=term}
+
+
+        
 let run (cg:Graph.t) (cfg:Cfg.t) : Cfg.t =
   let open SymConst in
   
@@ -167,7 +269,8 @@ let run (cg:Graph.t) (cfg:Cfg.t) : Cfg.t =
   let cp_block (l:Ll.lbl) (cfg:Cfg.t) : Cfg.t =
     let b = Cfg.block cfg l in
     let cb = Graph.uid_out cg l in
-    failwith "Constprop.cp_block unimplemented"
+    let b' = cons_block cb b in 
+    Cfg.add_block l b' cfg
   in
 
   LblS.fold cp_block (Cfg.nodes cfg) cfg
